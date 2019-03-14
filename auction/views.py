1b09922f8d86488f4833d957271c4951a5002030
Dheshan Mohandass <dheshan.mail@gmail.com>
2019-03-14 15:28:10 +0530

    Initial Commit  * Have Templates working

---
+++ auction/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# Dummy Data
+posts = [
+    {
+        'author': 'PyCharm',
+        'title': 'Django',
+        'content': 'Django is the award-winning leader of Python web frameworks and PyCharm has long supported it. '
+                   'Running, debugging, navigating, working productively... PyCharm has you covered for Django.',
+        'date_posted': 'January 4, 2019'
+    },
+    {
+        'author': 'PyCharm',
+        'title': 'Flask',
+        'content': 'The fast-growing Flask microframework has strong and growing PyCharm support: templates, '
+                   'navigation, completion, and more.',
+        'date_posted': 'January 5, 2019'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'auction/home.html', context)
+
+
+def about(request):
+    return render(request, 'auction/about.html')
